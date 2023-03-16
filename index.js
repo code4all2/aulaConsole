9214d9e324c3d6dc565544ff2ace76e7c594f19e
code4all2 <code4allaulaonline@gmail.com>
2023-03-16 10:09:18 -0300

    estudo console.log()

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log(1); //number
+console.log("Kaio"); // string <-palavra, letras ou alfa-numéricos
+console.log("1");
+console.log((18 + 1 )/ 10);
+console.log("K",18);
+// number inteiros = int
+// decimal = float 
+
+// multip  div
+// som sub
+
+// frase 
