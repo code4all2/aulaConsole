45b38f3972a533e2cd5424a7c951aaf21a4a9cbf
code4all2 <code4allaulaonline@gmail.com>
2023-03-23 10:04:29 -0300

    variaveis

---
+++ variaveis.js
@@ -0,0 +1,22 @@
+let nome;
+let sobreNome; // assim que escreve variaveis camelCase
+let sobrenome; // case sensitivity
+
+const n1 = 48;
+//console.log(n1);
+
+let nn1 = n1 + 1;
+//console.log(nn1)
+
+//calcula imc
+
+//nome sobrenome, idade, peso, altura
+
+
+//imc -> peso / (altura * altura);
+//ano de nascimento = ano atual - idade;
+const peso = 78;
+const altura = 1.74; 
+let imc;
+imc = peso / (altura * altura);
+console.log(imc);
